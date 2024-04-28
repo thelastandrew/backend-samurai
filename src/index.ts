13b84acf6a45966bf35a45f5d59ac28d33a2fba6
thelastandrew <thelastandrew@gmail.com>
2024-04-28 21:10:08 +0300

    feat: add get requests

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,50 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (_: Request, res: Response) => {
-  res.send('Hello Andrey!');
+const products = [
+  { id: 1, title: 'tomato' },
+  { id: 1, title: 'orange' },
+];
+const addresses = [
+  { id: 1, value: 'Moscow' },
+  { id: 2, value: 'Perm' },
+];
+
+app.get('/products', (req: Request, res: Response) => {
+  const { title } = req.query;
+  console.log('title', title);
+  if (!title) {
+    res.send(products);
+    return;
+  }
+
+  res.send(products.filter((p) => p.title.indexOf(title as string) > -1));
+});
+
+app.get('/products/:title', (req: Request, res: Response) => {
+  const product = products.find((p) => p.title === req.params.title);
+
+  if (!product) {
+    res.sendStatus(404);
+    return;
+  }
+
+  res.send(product);
+});
+
+app.get('/addresses', (_: Request, res: Response) => {
+  res.send(addresses);
+});
+
+app.get('/addresses/:id', (req: Request, res: Response) => {
+  const address = addresses.find((a) => a.id === Number(req.params.id));
+
+  if (!address) {
+    res.sendStatus(404);
+    return;
+  }
+
+  res.send(address);
 });
 
 app.listen(port, () => {
