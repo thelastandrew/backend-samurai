a6554743bfa648cd84348ac760fd05fb31068d8c
thelastandrew <thelastandrew@gmail.com>
2024-05-20 23:20:40 +0300

    feat: add nodemailer template

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,38 @@
+import nodemailer from 'nodemailer';
 import { app } from './app';
 import { connectToDb } from './db';
 import { settings } from './settings';
 
 const port = settings.PORT;
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: settings.EMAIL_ADDRESS,
+    pass: settings.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+const mailOptions = {
+  from: settings.EMAIL_ADDRESS,
+  to: 'xwerdx@mail.ru',
+  subject: 'Hello from Nodemailer',
+  text: 'hello from thelastandrew',
+};
+
+// transporter.sendMail(mailOptions, (error, info) => {
+//   if (error) {
+//     console.error('Error sending email: ', error);
+//   } else {
+//     console.log('Email sent: ', info.response);
+//   }
+// });
 
 const start = async () => {
   await connectToDb();
--- src/settings.ts
+++ src/settings.ts
@@ -5,4 +5,6 @@ export const settings = {
   PORT: process.env.PORT as string | 3000,
   DB_URI: process.env.DB_URI as string,
   JWT_SECRET: process.env.JWT_SECRET as string,
+  EMAIL_ADDRESS: process.env.EMAIL_ADDRESS as string,
+  EMAIL_PASS: process.env.EMAIL_PASS as string,
 };
